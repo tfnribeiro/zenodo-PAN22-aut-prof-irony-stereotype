2dc3d6fd7da37a8a907783db750e9430b14818c5
KSEHano <khano@uos.de>
2022-04-29 11:27:20 +0200

    fixed tweet to wordlist

--- utils.py
+++ utils.py
@@ -17,7 +17,6 @@ def tweet_to_wordlist(tweet):
     tweet = re.sub('(“|”)', '"', tweet)
     tweet = re.sub("(’|‘)", "'", tweet)
     tweet = re.sub("( '|' |…|&amp;)", "", tweet)
-    tweet = re.sub("(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])", "", tweet)
     tweet = re.sub('(\.|\?|\!|"|,|\(|\)|:)', "", tweet)
     
 
